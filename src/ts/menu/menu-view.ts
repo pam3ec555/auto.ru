e028940b93dff7da6f3815b2446c928315801080
Ramil Rakhmatullin <ramil@clientbase.ru>
2018-04-12 17:53:57 +0300

    menu-view...

--- src/ts/menu/menu-view.ts
+++ src/ts/menu/menu-view.ts
@@ -1,5 +1,29 @@
 const drawLogo = (): string => {
     return `<div class="logo">
-      <img src="img/icons/logo.svg" alt="Car">
-    </div>`;
+              <img src="img/icons/logo.svg" alt="Car">
+            </div>`;
+};
+
+const drawNav = (data: object): string => {
+    return `<ul class="nav__list">
+              <li class="nav__item">
+                <a href="#" class="nav__link">test</a>
+              </li>
+              <li class="nav__item">
+                <a href="#" class="nav__link">test</a>
+              </li>
+              <li class="nav__item">
+                <a href="#" class="nav__link">test</a>
+              </li>
+            </ul>`;
+};
+
+const drawMobileHeader = (): string => {
+    return `<header class="outer-block">
+              <button type="button" class="hamburger" id="menu-show">
+                <div class="hamburger__inner"></div>
+              </button>
+              ${drawLogo()}
+              <button type="button" class="search-btn" id="search-btn"></button>
+            </header>`;
 };
